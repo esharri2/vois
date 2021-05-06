1b7ce4b317f0e8d043dfc7669bdc5301b5fcb517
Evan Harrison <esharri2@gmail.com>
2021-05-05 20:29:45 -0400

    manifest stuff

--- config/passthroughs/index.js
+++ config/passthroughs/index.js
@@ -3,5 +3,5 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy({ "./_temp/main.css": "./main.css" });
   eleventyConfig.addPassthroughCopy({ "./_temp/main.js": "./main.js" });
   eleventyConfig.addPassthroughCopy({ "./assets/fonts/*": "./" });
-  eleventyConfig.addPassthroughCopy({ "./assets/favicons/*": "./" });
+  eleventyConfig.addPassthroughCopy({ "./assets/icons/*": "./" });
 };
